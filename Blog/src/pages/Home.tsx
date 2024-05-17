438bbdb689fb00c286b524bc0349b3340846f82e
Tanvir Singh <tanveersing559@gmail.com>
2024-05-17 20:22:49 +0530

    chlpya

--- Blog/src/pages/Home.tsx
+++ Blog/src/pages/Home.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 
 function Home() {
+    const navigate  = useNavigate()
+    useEffect(()=>{
+        navigate("/signup")
+    })
     return(
         <>
         <div>
